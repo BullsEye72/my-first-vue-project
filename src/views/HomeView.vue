6eff3edcb35f42bb39513986e6a55ff55bdca751
Anthony Jaspers <anthony.jaspers@letsgocoding.dev>
2024-10-11 16:04:04 +0200

    composable and localstorage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { useStorage } from '@/composables/useStorage'
+
+let food = useStorage('food', 'pizza')
+let age = useStorage('age', 30)
+let obj = useStorage('obj', { one: 'one' })
+
+setTimeout(() => {
+  obj.value.one = 'CHANGED'
+}, 3000)
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <p>What is your favorite food? <input type="text" v-model="food" /></p>
+    <p>What is your age? <input type="text" v-model="age" /></p>
   </main>
 </template>
